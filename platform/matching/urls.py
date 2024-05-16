a554b040842d7d3be10945ec531875e03ac40d95
Bai1026 <1132abc30@gmail.com>
2024-05-16 20:08:52 +0800

    update url

--- platform/matching/urls.py
+++ platform/matching/urls.py
@@ -7,13 +7,12 @@ from .filter import filter_function, show_all_profiles
 from login.views import UserProfileUpdate
 
 urlpatterns = [
+    path('<str:username>/', user_matching, name='user_matching'),
     path('filter/', filter_function, name='filter_function'),
     path('show-all-profiles/', show_all_profiles, name='show_all_profiles'),
     # path('matching/<str:username>/', user_matching, name='user_matching'),
-    path('<str:username>/', user_matching, name='user_matching'),
     path('hide-profile/<str:username>/', hide_profile, name='hide_profile'),
     path('delete/<username>/', delete_profile, name='delete_profile'),    
     path('matching-list/', matching_list, name='matching_list'),
-
     path('profile_update/<str:username>/', UserProfileUpdate.as_view(), name='update_profile'),
 ]
